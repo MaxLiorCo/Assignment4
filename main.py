24ca771de8a77f42d0eadece1d4a5e4d3a1eae83
Lior Fradin <khtur9512300@walla.com>
2021-01-10 19:59:25 +0200

    V1.00003

---
+++ main.py
@@ -0,0 +1,15 @@
+import sqlite3
+import os
+import sys
+from Repository import _Repository
+from sqlite3 import Error
+
+args = sys.argv
+conn = None
+
+conn = sqlite3.connect("./database.db")
+repo = _Repository()
+repo.create_tables()
+repo.registerFile(sys.argv[0])
+repo.executeOrders(sys.argv[1])
+# inside executeOrders we create output file
