93e84aaeefc58d83d91bd4812a342323f87128ac
Lior Fradin <khtur9512300@walla.com>
2021-01-11 22:39:53 +0200

    final version [lease

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import sys
 from Repository import repo
 from sqlite3 import Error
 
-conn = sqlite3.connect("./database.db")
 
 try:
     repo.create_tables()
+    repo.register_file(sys.argv[1])
+    repo.execute_orders(sys.argv[2])
 except Error as e:
     pass
-repo.register_file(sys.argv[1])
-repo.execute_orders(sys.argv[2])
