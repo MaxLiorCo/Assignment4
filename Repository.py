331e3ecf9c5a194de49e51b147830c60ba15810d
Lior Fradin <khtur9512300@walla.com>
2021-01-11 17:07:36 +0200

    maxim kah ya zain

--- Repository.py
+++ Repository.py
@@ -81,20 +81,30 @@ class _Repository:
             # read first line to know the lengths of the insertions for each table
             tableInsertionsLength = [int(num) for num in firstLine.split(',')]
             # NOTICE: last cell in each line is "_\n" but python ignores \n when converting to int, would produce bugs otherwise
+
+            # vaccines
             for i in range(tableInsertionsLength[0]):
                 line = file_reader.readline().split(',')
+                self.vaccines.total_inventory += int(line[3])  # adds given quantity
                 self.vaccines.insert(
                     Vaccine(int(line[0]), datetime.strptime(line[1], "%Y-%m-%d").date(), int(line[2]), int(line[3])))
+            # suppliers
             for i in range(tableInsertionsLength[1]):
                 line = file_reader.readline().split(',')
                 self.suppliers.insert(Supplier(int(line[0]), line[1], int(line[2])))
+            # clinics
             for i in range(tableInsertionsLength[2]):
                 line = file_reader.readline().split(',')
+                self.clinics.total_demand += int(line[2])  # adds given demand
                 self.clinics.insert(Clinic(int(line[0]), line[1], int(line[2]), int(line[3])))
+            # logistics
             for i in range(tableInsertionsLength[3]):
                 line = file_reader.readline().split(',')
+                self.logistics.total_received += int(line[3])
+                self.logistics.total_sent += int(line[2])
                 self.logistics.insert(Logistic(int(line[0]), line[1], int(line[2]), int(line[3])))
 
+            # TODO remove this before submission
             curser = self._conn.cursor()
             print(curser.execute(""" 
                 SELECT * FROM vaccines
@@ -109,24 +119,27 @@ class _Repository:
                 SELECT * FROM logistics
             """).fetchall())
 
+    def add_summary_line(self):
+        with open('./output.txt', 'a') as the_file:
+            the_file.write(str(self.vaccines.total_inventory) + "," +
+                           str(self.clinics.total_demand) + "," +
+                           str(self.logistics.total_received) + "," +
+                           str(self.logistics.total_sent))
 
-    def add_summary_line(self, ):
     # ------------------------------------------------------------------------------------------------------------
     # Executing orders
     def execute_orders(self, filePath):
         with open(filePath, 'r') as file_reader:
             for line in file_reader:
-                result = [line.strip() for x in line.split(',')] # splits the string where the comma is
+                result = [line.strip() for x in line.split(',')]  # splits the string where the comma is
                 if len(result) == 3:
                     receive_shipment(result[0], result[1], result[2])
-                #else:
+                    # else:
                     send_shipment(result[0], result[1])
 
     def receive_shipment(self, name, amount, date):
         id = _Vaccines.order_count
         date_to_insert = datetime.strptime(date, "%Y-%m-%d").date()
-
-
 
 # Create repository singleton
 # repo = _Repository()
